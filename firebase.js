735fbd5d2174ab781747f2701e8652b445c374b3
olawaleprayer-cyber <olawaleprayer@gmail.com>
2026-08-15 07:46:19 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-  import { getFirestore, doc, setDoc, getDoc, collection, addDoc, updateDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCiX9H4H_sR_pCIUoJ1NQOSISC221qQDyw",
-    authDomain: "growmore-investments.firebaseapp.com",
-    projectId: "growmore-investments",
-    storageBucket: "growmore-investments.appspot.com",
-    messagingSenderId: "633069707266",
-    appId: "1:633069707266:web:de27c2071f4f2bb5e7071b"
-  };
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
-  export { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, doc, setDoc, getDoc, collection, addDoc, updateDoc, onSnapshot };
-</script>
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut };
